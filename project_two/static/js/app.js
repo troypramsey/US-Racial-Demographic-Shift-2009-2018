b9a290ec09bb7bd02ffefb76e19b02a973a6d446
Troy Ramsey <troypramsey@gmail.com>
2020-10-27 15:45:26 -0700

    resolved promises conflict

--- project_two/static/js/app.js
+++ project_two/static/js/app.js
@@ -11,44 +11,78 @@ let svg = d3.select('#map')
     .append('svg')
     .attr('height', height)
     .attr('width', width)
-    .append('g')
 
-let data2011;
+// let population = d3.map()
 
-function getColor(id, dataset) {
-    let d = (Object.values(dataset[id]['%_white']))
-    d>90?'red':
-    d>70?'orange':
-    d>50?'yellow':
-    d>30?'green':
-    'blue'
-    console.log(d)
-    console.log(dataset)
-}
+// var path = d3.geoPath();
 
-d3.json('./static/js/us.json').then(data => {
-    console.log(data)
+var x = d3.scaleLinear()
+    .domain([1, 10])
+    .rangeRound([600, 860]);
+
+var color = d3.scaleThreshold()
+    .domain(d3.range(2, 10))
+    .range(d3.schemeBlues[9]);
+
+var g = svg.append("g")
+    .attr("class", "key")
+    .attr("transform", "translate(0,40)");
+
+g.selectAll("rect")
+  .data(color.range().map(function(d) {
+      d = color.invertExtent(d);
+      if (d[0] == null) d[0] = x.domain()[0];
+      if (d[1] == null) d[1] = x.domain()[1];
+      return d;
+    }))
+  .enter().append("rect")
+    .attr("height", 8)
+    .attr("x", function(d) { return x(d[0]); })
+    .attr("width", function(d) { return x(d[1]) - x(d[0]); })
+    .attr("fill", function(d) { return color(d[0]); });
+
+g.append("text")
+    .attr("class", "caption")
+    .attr("x", x.range()[0])
+    .attr("y", -6)
+    .attr("fill", "#000")
+    .attr("text-anchor", "start")
+    .attr("font-weight", "bold")
+    .text("White Population (%)");
+
+g.call(d3.axisBottom(x)
+    .tickSize(13)
+    .tickFormat(function(x, i) { return i ? x : x + "%"; })
+    .tickValues(color.domain()))
+  .select(".domain")
+    .remove();
+
+let promises = [
+    d3.json('./static/js/us.json'),
+    d3.json('./static/js/states2011.json')
+]
+
+Promise.all(promises).then(data => {
     let projection = d3.geoAlbersUsa()
-    .translate([width/2, height/2])
-    .scale(1000)
+        .translate([width/2, height/2])
+        .scale(1000)
 
-    let path = d3.geoPath()
-        .projection(projection)
+        let path = d3.geoPath()
+            .projection(projection)
 
-    let states = topojson.feature(data, data.objects.states).features
-    console.log(states)
-
-    d3.json('./static/js/states2011.json').then(data => {
-        data2011 = data
-        console.log(data2011)
+        let states = topojson.feature(data[0], data[0].objects.states).features
+        console.log(states)
 
         svg.selectAll('.state')
         .data(states)
         .join('path')
         .classed('state', true)
         .attr('d', path)
-        .attr('fill', getColor(states.id, data2011))
-        })
-    })
+
+        console.log(data[1])
+}
+)
+
+
     
     
