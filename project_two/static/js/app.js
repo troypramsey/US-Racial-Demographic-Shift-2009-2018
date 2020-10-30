8cf09a7abcbca1e9d0f4e6bd40c66767a95abd00
Troy Ramsey <troypramsey@gmail.com>
2020-10-30 14:27:20 -0700

    commenting js for better organization when rewriting code to function format

--- project_two/static/js/app.js
+++ project_two/static/js/app.js
@@ -1,3 +1,6 @@
+// Map size/margin values
+let height = 800, width = 1000
+
 let margin = {
     top: 0,
     left: 0,
@@ -5,17 +8,20 @@ let margin = {
     bottom: 0
 }
 
+// DROPDOWN
+// Values for building dropdown
 let years = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018]
 
-let height = 800, width = 1000
-
+// Default year on page initialization
 let defaultOption = 2009
 
+// D3 to build dropdown
 let dropdown = d3.select('#dropdown-container')
     .append('select')
     .classed('form-control form-control-lg', true)
     .attr('id', 'dropdown')
 
+// D3 to build selection for each year
 let selections = dropdown.selectAll('option')
     .data(years)
     .join('option')
@@ -23,65 +29,78 @@ let selections = dropdown.selectAll('option')
     .attr('class', 'year')
     .text(d => d)
 
+// Draw initial map
 drawMap(defaultOption)
 
+// Event listener for change in year
 dropdown.on('change', function() {
     year = dropdown.node().value
     drawMap(year)
 })
 
-
-let tooltip = d3.select('body')
-    .append('div')
-    .attr('id', 'tooltip')
-    .attr('style', 'position: absolute; visibility: hidden; background: #fafafa; padding: 2px; text-align: center; width: 200px;')
-    .style('left', '900px')
-
-let countyCard = d3.select('#county-card')
-
+// Select the map object
 let svg = d3.select('#map')
     .append('svg')
     .attr('height', height)
     .attr('width', width)
 
-let zoom = d3.zoom()
-    .scaleExtent([1, 8])
-    .on('zoom', zoomed);
-
-function zoomed() {
-    svg
-        .selectAll('path') // To prevent stroke width from scaling
-        .attr('transform', d3.event.transform);
-    }
-
-svg.call(zoom)
-
-
-
+// DRAW MAP FUNCTION
+// Pass in year from dropdown
 function drawMap(year) {
+    // TopoJSON object data
     d3.json('./static/js/us.json').then(data => {
     let mapData = data
+
+    // Build data query using selected year
     let dataQuery = './data/census' + year + '.json'
 
+    // Use query to pull in population data
     d3.json(dataQuery).then(data => {
         let popData = data
 
+        // Color scale function
         let color = d3.scaleLinear()
         .domain([1,35])
         .range(['#FAFAFA', '#2DC200']);
 
-   
+        // Build tooltip with initial hidden visibility
+        let tooltip = d3.select('body')
+        .append('div')
+        .attr('id', 'tooltip')
+        .attr('style', 'position: absolute; visibility: hidden; background: #4F4F4F; padding: 2px; text-align: center; width: 200px; color: #FAFAFA; opacity: 0.8; border-radius: 10px;')
+
+        // County card displays static county information on click
+        let countyCard = d3.select('#county-card')
+
+        // Building zoom function
+        let zoom = d3.zoom()
+            .scaleExtent([1, 8])
+            .on('zoom', zoomed);
+
+        function zoomed() {
+            svg
+                .selectAll('path') // To prevent stroke width from scaling
+                .attr('transform', d3.event.transform);
+            }
+
+        // Calling zoom function on map
+        svg.call(zoom)
+
+        // Map projection object
         let projection = d3.geoAlbersUsa()
         .translate([width/2, height/2])
         .scale(1000)
 
+        // Path constructor takes in county and state data from TopoJSON object
         let path = d3.geoPath()
             .projection(projection)
 
+            // County borders from TopoJSON
             let counties = topojson.feature(mapData, mapData.objects.counties).features
-
+            // State borders from TopoJSON
             let states = topojson.feature(mapData, mapData.objects.states).features
        
+            // D3 binds data from population datasets to TopoJSON polygons
             svg.selectAll('.county')
             .data(counties)
             .join('path')
@@ -101,6 +120,7 @@ function drawMap(year) {
                 }
                 return color(percentage)
             })
+            // Unhides tooltip on mouseover
             .on('mouseover', function(countyItem)  {
                 tooltip.transition()
                     .style('visibility', 'visible')
@@ -132,12 +152,14 @@ function drawMap(year) {
 
                 tooltip.html(name + ', ' + stateName)
             })
+            // Re-hides tooltip on mouseout
             .on('mouseout', function (countyItem)  {
                 tooltip.transition()
                     .style('visibility', 'hidden')
 
                     
             })
+            // Updates information in card on click
             .on('click', function(countyItem)  {
                 
                 let fips = countyItem.id
@@ -162,9 +184,10 @@ function drawMap(year) {
                     asian = 'Unknown'
                 }
 
-                countyCard.html(year + '<br>' + name + ', ' + stateName + '<br>' + percentage + '% nonwhite' + '<br>' + 'Black: ' + black + '%<br>' + 'Latinx: ' + latinx + '%<br>' + 'Native American: ' + native + '%<br>' + 'Asian-American: ' + asian + '%')
+                countyCard.html(year + '<br>' + '<h3>' + name + ', ' + stateName + '</h3><br>Nonwhite: ' + percentage + '%<br>' + 'Black: ' + black + '%<br>' + 'Hispanic or Latino: ' + latinx + '%<br>' + 'Native American: ' + native + '%<br>' + 'Asian-American: ' + asian + '%')
             })
             
+            // Draws state borders on top of county map
             svg.selectAll('.state')
             .data(states)
             .join('path')
@@ -175,12 +198,6 @@ function drawMap(year) {
 
     })
 
-        
-
-        
-        // .attr('fill', function(d) {
-        //     return color(data[1].pct_white = data[1].fips.get(d.id))
-        // })
 }
 )
 }
