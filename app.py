56174c4bf493cefc7b806eaf47931531521ffc85
Rahil Nath <rahilanath@gmail.com>
2020-10-30 00:05:48 -0700

    app.py and stuff

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from pymongo import MongoClient
+from bson.json_util import dumps
 
 app = Flask(__name__)
 
-mongo = MongoClient('mongodb+srv://dbadmin:GroupProject2@groupproject2.ocbpa.mongodb.net/sample_airbnb?retryWrites=true&w=majority')
+mongo = MongoClient('mongodb+srv://readuser:readpassword@groupproject2.ocbpa.mongodb.net/test?retryWrites=true&w=majority')
+db = mongo['static']
+collection = db['json_data']
 
-@app.route('/framework', methods=['GET'])
-def get_all_frameworks():
-    framework = mongo.db.framework
+@app.route('/by_year/<year>', methods=['GET'])
+def year(year):
+    results = list(collection.find({'year':int(year)}))
+    return app.response_class(dumps(results), mimetype="application/json")
 
-    output = []
-
-    for q in framework.find():
-        output.append({'name' : q[name]})
-
-    return jsonify({'result' : output})
-
+@app.route('/by_state_name/<state_name>', methods=['GET'])
+def state_name(state_name):
+    results = list(collection.find({'state_name':state_name}))
+    return app.response_class(dumps(results), mimetype="application/json")
+    
 if __name__ == '__main__':
     app.run(debug=True)
