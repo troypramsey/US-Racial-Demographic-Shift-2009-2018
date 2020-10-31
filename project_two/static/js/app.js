156aad4c5bb8c231efe04a0ee52b24f24d2da8b4
Troy Ramsey <troypramsey@gmail.com>
2020-10-30 21:40:42 -0700

    routed data from mongo to app.js

--- project_two/static/js/app.js
+++ project_two/static/js/app.js
@@ -42,19 +42,23 @@ let svg = d3.select('#map')
 // Pass in year from dropdown
 function drawMap(year) {
     // TopoJSON object data
-    d3.json('./static/js/us.json').then(data => {
+    d3.json('../static/js/us.json').then(data => {
     let mapData = data
 
     // Build data query using selected year
-    let dataQuery = './data/census' + year + '.json'
+
+    // dataQuery = /by_year/${year}
+
+    // let dataQuery = 'http://127.0.0.1:5000/by_year/' + year + '/'
 
     // Use query to pull in population data
-    d3.json(dataQuery).then(data => {
+    d3.json(`/by_year/${year}`).then(data => {
+        
         let popData = data
 
         // Color scale function
         let color = d3.scaleLinear()
-        .domain([1,35])
+        .domain([1,30])
         .range(['#FAFAFA', '#2DC200']);
 
         // Build tooltip with initial hidden visibility
@@ -62,7 +66,8 @@ function drawMap(year) {
         .append('div')
         .attr('id', 'tooltip')
         .attr('style', 'position: absolute; visibility: hidden; background: #4F4F4F; padding: 2px; text-align: center; width: 200px; color: #FAFAFA; opacity: 0.8; border-radius: 10px; border: .5px solid #FAFAFA')
-
+        .attr('html', '')
+        
         // County card displays static county information on click
         let countyCard = d3.select('#county-card')
 
@@ -90,105 +95,95 @@ function drawMap(year) {
             .projection(projection)
 
             // County borders from TopoJSON
-            let counties = topojson.feature(mapData, mapData.objects.counties).features
-            // State borders from TopoJSON
-            let states = topojson.feature(mapData, mapData.objects.states).features
-       
-            // D3 binds data from population datasets to TopoJSON polygons
-            svg.selectAll('.county')
-            .data(counties)
-            .join('path')
-            .classed('county', true)
-            .attr('d', path)
-            .attr('fill', function(d) {
-                let id = d.id
-                let county = popData.find((item) => {
-                    return item.fips === id
-                })
-                let percentage
-                try {
-                    percentage = county['nonwhite_pct']
-                }
-                catch (err) {
-                    percentage = 1
-                }
-                return color(percentage)
+        let counties = topojson.feature(mapData, mapData.objects.counties).features
+        // State borders from TopoJSON
+        let states = topojson.feature(mapData, mapData.objects.states).features
+    
+        // D3 binds data from population datasets to TopoJSON polygons
+        svg.selectAll('.county')
+        .data(counties)
+        .join('path')
+        .classed('county', true)
+        .attr('d', path)
+        .attr('fill', function(d) {
+            let id = d.id
+            let county = popData.find((item) => {
+                return item.fips === id
             })
-            // Unhides tooltip on mouseover
-            .on('mouseover', function(countyItem)  {
-                tooltip.transition()
-                    .style('visibility', 'visible')
-                    .style('left', (d3.event.pageX+40) + 'px')
-                    .style('top', (d3.event.pageY -28) + 'px')
-                let fips = countyItem.id
-                let county = popData.find((item) => {
-                    return item.fips === fips
-                })
-                let name, percentage
-                try {
-                    name = county.county_name
-                    stateName = county.state_name
-                    percentage = county.nonwhite_pct
-                    black = county.black_pct
-                    latinx = county.latinx_pct
-                    native = county.native_pct
-                    asian = county.asian_pct
-                }
-                catch (err) {
-                    name = 'Data Missing From Census'
-                    stateName = 'Unknown'
-                    percentage = 'Unknown'
-                    black = 'Unknown'
-                    latinx = 'Unknown'
-                    native = 'Unknown'
-                    asian = 'Unknown'
-                }
+            let percentage
+            try {
+                percentage = county['nonwhite_pct']
+            }
+            catch (err) {
+                percentage = 1
+            }
+            return color(percentage)
+        })
+        // Unhides tooltip on mouseover
+        .on('mouseover', function(countyItem)  {
+            tooltip.transition()
+                .style('visibility', 'visible')
+                .style('left', (d3.event.pageX+40) + 'px')
+                .style('top', (d3.event.pageY -28) + 'px')
+            let fips = countyItem.id
+            let county = popData.find((item) => {
+                return item.fips === fips
+            })
+            let name, percentage
+            try {
+                name = county.county_name
+                stateName = county.state_name
+            }
+            catch (err) {
+                name = 'Data Missing From Census'
+                stateName = 'Unknown'
+            }
 
-                tooltip.html(name + ', ' + stateName)
-            })
-            // Re-hides tooltip on mouseout
-            .on('mouseout', function (countyItem)  {
-                tooltip.transition()
-                    .style('visibility', 'hidden')
+            tooltip.html(name + ', ' + stateName)
+        })
+        // Re-hides tooltip on mouseout
+        .on('mouseout', function (countyItem)  {
+            tooltip.transition()
+                .style('visibility', 'hidden')
 
-                    
-            })
-            // Updates information in card on click
-            .on('click', function(countyItem)  {
                 
-                let fips = countyItem.id
-                let county = popData.find((item) => {
-                    return item.fips === fips
-                })
-                let name, percentage
-                try {
-                    name = county.county_name
-                    percentage = county.nonwhite_pct
-                    black = county.black_pct
-                    latinx = county.latinx_pct
-                    native = county.native_pct
-                    asian = county.asian_pct
-                }
-                catch (err) {
-                    name = 'Data Missing From Census'
-                    percentage = 'Unknown'
-                    black = 'Unknown'
-                    latinx = 'Unknown'
-                    native = 'Unknown'
-                    asian = 'Unknown'
-                }
-
-                countyCard.html('<h3>' + name + ', ' + stateName + '</h3><br>Nonwhite: ' + percentage + '%<br>' + 'Black: ' + black + '%<br>' + 'Hispanic or Latino: ' + latinx + '%<br>' + 'Native American: ' + native + '%<br>' + 'Asian-American: ' + asian + '%')
-            })
+        })
+        // Updates information in card on click
+        .on('click', function(countyItem)  {
             
-            // Draws state borders on top of county map
-            svg.selectAll('.state')
-            .data(states)
-            .join('path')
-            .classed('state', true)
-            .attr('d', path)
-            .attr('stroke-width', '2')
-            .attr('fill', 'none')
+            let fips = countyItem.id
+            let county = popData.find((item) => {
+                return item.fips === fips
+            })
+            let name, percentage
+            try {
+                name = county.county_name
+                percentage = county.nonwhite_pct
+                black = county.black_pct
+                latinx = county.latinx_pct
+                native = county.native_pct
+                asian = county.asian_pct
+            }
+            catch (err) {
+                name = 'Data Missing From Census'
+                percentage = 'Unknown'
+                black = 'Unknown'
+                latinx = 'Unknown'
+                native = 'Unknown'
+                asian = 'Unknown'
+            }
+
+            countyCard.html('<h3>' + name + ', ' + stateName + '</h3><br><h5>Nonwhite: ' + percentage + '%</h5><br>' + '- Black: ' + black + '%<br>' + '- Hispanic or Latino: ' + latinx + '%<br>' + '- Native American: ' + native + '%<br>' + '- Asian-American: ' + asian + '%')
+        })
+        
+        // Draws state borders on top of county map
+        svg.selectAll('.state')
+        .data(states)
+        .join('path')
+        .classed('state', true)
+        .attr('d', path)
+        .attr('stroke-width', '2')
+        .attr('fill', 'none')
 
     })
 
